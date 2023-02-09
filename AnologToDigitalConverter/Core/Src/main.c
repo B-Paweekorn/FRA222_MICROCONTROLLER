0e29538481e40234842d75b93ad29e8f64943d06
chawrewerwee <paweekorn.pb@gmail.com>
2023-02-09 10:27:36 +0700

    .

--- AnologToDigitalConverter/Core/Src/main.c
+++ AnologToDigitalConverter/Core/Src/main.c
@@ -39,18 +39,27 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+typedef struct{
+	ADC_ChannelConfTypeDef Config;
+	uint32_t Data;
+}ADCStructure;
 
+ADCStructure ADCConvert[3] = {0};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
-
+void ADC_Read();
+void ADC_Config();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -87,14 +96,16 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  ADC_Config();
   while (1)
   {
+	  ADC_Read();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -146,6 +157,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -215,6 +278,40 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+void ADC_Config(){
+	ADCConvert[0].Config.Channel = ADC_CHANNEL_0;
+	ADCConvert[0].Config.Rank = 1;
+	ADCConvert[0].Config.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+	ADCConvert[1].Config.Channel = ADC_CHANNEL_1;
+	ADCConvert[1].Config.Rank = 1;
+	ADCConvert[1].Config.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+	ADCConvert[2].Config.Channel = ADC_CHANNEL_TEMPSENSOR;
+	ADCConvert[2].Config.Rank = 1;
+	ADCConvert[2].Config.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+}
+void ADC_Read(){
+
+	register int i;
+	for(i = 0;i < 3;++i){
+	//setting adc
+
+		HAL_ADC_ConfigChannel(&hadc1 , &ADCConvert[i].Config);
+
+		HAL_ADC_Start(&hadc1);
+
+		if(HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK){
+			ADCConvert[i].Data = HAL_ADC_GetValue(&hadc1);
+		}
+		HAL_ADC_Stop(&hadc1);
+	}
+}
+
+
+
 
 /* USER CODE END 4 */
 
